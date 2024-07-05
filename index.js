ce2179f83a3e272f665d37926fa38196afa7487c
Muhammed Salim <Muhammed.Salim@windstream.com>
2024-07-05 18:12:01 +0530

    logrocket

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import LogRocket from '@logrocket/react-native';
+
+// add logrocket '<APP_SLUG>' here
+LogRocket.init('40ii7i/osl')
 
 AppRegistry.registerComponent(appName, () => App);
